80b0331d178b874999a92dfcea9f25067aff979c
pedrovt <pedro.teix@ua.pt>
2017-11-19 23:12:23 +0000

    Adicionado Guião 9

---
+++ P3/g9/iceCreamsManagement/Gelado.java
@@ -0,0 +1,17 @@
+package g9.iceCreamsManagement;
+
+/**
+ * GeladoInterface
+ * 
+ * @author Pedro Teixeira, 84715, MIECT
+ */
+public interface Gelado {
+
+	/**
+	 * 
+	 * @param num
+	 */
+	public abstract void base(int num);
+	
+	
+}
---
+++ P3/g9/iterators/resources/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * package-info
+ * 
+ * @author Pedro Teixeira, 84715, MIECT
+ */
+package g9.iterators.resources;
